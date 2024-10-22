7f84a25f569a152ff2cb1afb964f40acc922846a
marina6coneto <santoscmarina@hotmail.com>
2024-10-22 11:04:43 -0300

    add view sistema adocao

--- animal_shelter/shelter/urls.py
+++ animal_shelter/shelter/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AnimalListView
+from .views import AnimalListView, AdoptionRequestView
 
 urlpatterns = [
   path('', AnimalListView.as_view(), name='animal_list'),
+  path('adoption/<int:animal_id>/', AdoptionRequestView.as_view(), name='adoption_request'),
 ]
--- animal_shelter/shelter/views.py
+++ animal_shelter/shelter/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Animal
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, FormView
+from .models import Animal, Adoption
 from django.db.models import Q
+from .forms import AdoptionForm
+from django.http import HttpResponse
 
 
 class AnimalListView(ListView):
@@ -28,4 +30,29 @@ class AnimalListView(ListView):
         context = super().get_context_data(**kwargs)
         # Include the search query in the context
         context["search_query"] = self.request.GET.get("search_term", "")
+        return context
+
+
+class AdoptionRequestView(FormView):
+    template_name = 'adoption_form.html' 
+    form_class = AdoptionForm
+
+    def form_valid(self, form):
+        
+        animal_id = self.kwargs['animal_id']
+        animal = get_object_or_404(Animal, id=animal_id)
+
+        
+        adoption = form.save(commit=False)
+        adoption.animal = animal  
+        adoption.save()  
+
+        return HttpResponse("Adoção solicitada com sucesso! Aguardando aprovação.")
+
+    def get_context_data(self, **kwargs):
+        
+        context = super().get_context_data(**kwargs)
+        animal_id = self.kwargs['animal_id']
+        animal = get_object_or_404(Animal, id=animal_id)
+        context['animal'] = animal
         return context
