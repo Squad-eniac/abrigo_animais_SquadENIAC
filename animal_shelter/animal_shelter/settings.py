4fa8a44043aef41c56f808b50e0824a91e19aaaf
Lay Cipriano <webdevlay@gmail.com>
2024-10-27 01:59:25 -0300

    added new dependencies, superuser in db, new apps in settings and static files configured. Base template developed (search, navbar and footer)

--- animal_shelter/animal_shelter/settings.py
+++ animal_shelter/animal_shelter/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "shelter"
+    "shelter",
+    "django_bootstrap5"
 ]
 
 MIDDLEWARE = [
@@ -116,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -125,3 +126,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = '/media/'  # URL para acessar arquivos de mídia
 MEDIA_ROOT = BASE_DIR / 'media'  # Caminho absoluto onde as imagens serão armazenadas
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
